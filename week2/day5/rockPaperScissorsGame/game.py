0b0ac7abc0bb20b9157bb9d0cfb8a5adbbb36dff
silvia denanni <silvia.den@icloud.com>
2025-11-22 19:48:34 +0200

    Rock paper scissors mini Project

---
+++ week2/day5/rockPaperScissorsGame/game.py
@@ -0,0 +1,54 @@
+import random
+
+class Game:
+    def get_user_item(self):
+        user_item = input("Please choose your move: rock, paper or scissors? ")
+        return user_item
+    
+    def get_computer_item(self):
+        rand = random.randint(1,3)
+        if rand == 1:
+            return "rock"
+        elif rand == 2:
+            return "paper"
+        else:
+            return "scissors"
+    
+    def get_game_result(self, user_item, computer_item):
+        if user_item == computer_item:         #we take care of the more specific case, the "global" one
+            return "We have a tie"
+        
+        if user_item == "rock":                #nested ifs because each if block is independent from the other
+            if computer_item == "scissors":
+                return "You win!"
+            else:
+                return "You lose!"
+        
+        if user_item == "paper":
+            if computer_item == "rock":
+                return "You win!"
+            else:
+                return "You lose!"
+        
+        if user_item == "scissors":
+            if computer_item == "paper":
+                return "You win!"
+            else:
+                return "You lose!"
+
+    def play(self):
+        user_item = self.get_user_item()
+        computer_item = self.get_computer_item()
+        game_result = self.get_game_result(user_item, computer_item)
+        print(user_item)
+        print(computer_item)
+        print(game_result)       # return the result so main() in rock-paper-scissors.py can use it
+
+
+game1 = Game()
+result = game1.play()        # play() is a method, so you need to CALL IT WITH PARENTHESES to execute it 
+
+
+
+
+
---
+++ week2/day5/rockPaperScissorsGame/rock-paper-scissors.py
@@ -0,0 +1,56 @@
+from game import Game
+
+def get_user_menu_choice():
+    menu_options = ("Play a new game","Show scores", "Quit")
+
+    print("Menu options: ")
+    for option in menu_options:
+        print(f"{option}")      #show the menu options to the user
+
+    user_input = input("Please enter you option: ")
+
+    if user_input not in menu_options:
+        raise ValueError ("Enter only one of the menu options!")
+    else:
+        return user_input
+     
+def print_results(results):
+    wins = results.get("win", 0)
+    losses = results.get("loss", 0)
+    draws = results.get("draw", 0)
+
+    print(f" There are {wins} wins, {losses} losses and {draws} draws.")
+
+    print("Thank you for playing!")
+
+
+def main():
+    results = {"win": 0, "loss": 0, "draw": 0}
+
+    while True:
+        choice = get_user_menu_choice()          #storing in variable "choice" the function  
+
+        if choice == "Play a new game":
+            game1 = Game                         # instantiate the new game
+            result = game1.play()                  #storing in variable "result" the game + CALLING THE METHOD ()!!!
+
+            if result == "You win!":
+                results["win"] += 1           
+            elif result == "You lose!":
+                results ["loss"] += 1
+            else:
+                results ["draw"] += 1
+
+        elif choice == "Show scores":
+            print_results(results)
+        
+        elif choice == "Quit":
+            print_results(results)
+            print("Goodbye!")
+            break
+
+
+
+
+        
+                         
